c3054de603e29eed9353551894dedba28f1bfdca
Gabriel Carrizo <gabriel.carrizo@now.se>
2018-02-13 14:36:23 +0100

    progress

--- g_rbf.py
+++ g_rbf.py
@@ -50,10 +50,9 @@ def seq_learn(x,label,mu,t, eta):
 
     f_hat = np.dot(W,phi_x)
 
-
     return f_hat, W
 
-def dist_(x, W,r=.5):
+def dist_(x, W, r=.5):
     temp = np.subtract.outer(x,W)
     d = np.multiply(temp,temp).squeeze()
     in_radius = np.abs(d-np.min(d)).T<r
@@ -66,19 +65,17 @@ def clearning(x,mu_cl,t=50,eta=0.2):
         mu_cl += eta*np.multiply(in_radius,x_samp-mu_cl)
     return mu_cl
 
-if __name__ == "__main__":
-    x, valid, label = generateData()
-    t = 100 #number of epochs
-    eta = 0.2 #step size, learning rate
-    #mu = np.random.uniform(low=0, high=np.pi*2, size=(10,))
-    mu = np.linspace(0,2*np.pi,40)
-    f_hat_b, W_b = batch_train(x, label, mu)
-    f_hat_seq, W_seq= seq_learn(x,label,mu,t,eta)
-    mu_cl = np.copy(mu)
-    mu_cl = clearning(x,mu_cl)
-    f_hat_b_cl, W_b_cl = batch_train(x, label, mu_cl)
-    f_hat_seq_cl, W_seq_cl = seq_learn(x,label,mu_cl,t,eta)
+def winner(x,mu,fac=5):
+    winners = np.argmin(dist_(x,mu)[0],1)
+    wins = np.zeros(mu.shape)
+    for w in winners:
+        wins[w] += 1
 
+    wins = wins*fac
+
+    return wins
+
+def loadData():
     data = []
     with open('data_lab2/ballist.dat', 'r') as f:
         next = f.readline()
@@ -87,7 +84,48 @@ if __name__ == "__main__":
             list = [float(i) for i in list]
             data.append(list)
             next = f.readline()
-    data = np.array(data)
+
+    train = np.copy(data)
+    train_data = train[:,0:2]
+    train_labels = train[:,2:4]
+
+    data = []
+    with open('data_lab2/balltest.dat', 'r') as f:
+        next = f.readline()
+        while next != "":
+            list = next.replace('\t',' ').replace('\n', '').split(' ')
+            list = [float(i) for i in list]
+            data.append(list)
+            next = f.readline()
+
+    test = np.copy(data)
+    test_data = test[:,0:2]
+    test_labels = test[:,2:4]
+
+    return train_data, train_labels, test_data, test_labels
+
+def initMu(n, d):
+    for i in range(t):
+        x_samp = np.random.choice(x)
+        d, in_radius = dist_(x_samp,mu_cl,r=1)
+        mu_cl += eta*np.multiply(in_radius,x_samp-mu_cl)
+    return mu_cl
+
+def assignment1():
+    x, valid, label = generateData()
+    t = 100 #number of epochs
+    eta = 0.2 #step size, learning rate
+    mu = np.random.uniform(low=0, high=np.pi*2, size=(6,))
+    #mu = np.linspace(0,2*np.pi,6)
+    f_hat_b, W_b = batch_train(x, label, mu)
+    winners = winner(x,mu)
+
+    f_hat_seq, W_seq = seq_learn(x,label,mu,t,eta)
+    mu_cl = np.copy(mu)
+    mu_cl = clearning(x,mu_cl)
+    winners_cl = winner(x,mu_cl)
+    f_hat_b_cl, W_b_cl = batch_train(x, label, mu_cl)
+    f_hat_seq_cl, W_seq_cl = seq_learn(x,label,mu_cl,t,eta)
 
     phi_x = phi_i(valid,mu)
     f_hat_b_test = np.dot(W_b,phi_x)
@@ -100,25 +138,25 @@ if __name__ == "__main__":
     plt.subplot(2,2,1)
     plt.plot(x,f_hat_b,label="Predicted")
     plt.plot(x,label,label="True")
-    plt.scatter(mu, np.zeros(mu.shape))
+    plt.scatter(mu, np.zeros(mu.shape),s=winners)
     plt.title('f_hat_batch')
 
     plt.subplot(2,2,2)
     plt.plot(x,f_hat_seq,label="Predicted")
     plt.plot(x,label, label="True")
-    plt.scatter(mu,np.zeros(mu.shape))
+    plt.scatter(mu,np.zeros(mu.shape),s=winners)
     plt.title('f_hat_seq')
 
     plt.subplot(2,2,3)
     plt.plot(x,f_hat_b_cl,label="Predicted")
     plt.plot(x,label, label="True")
-    plt.scatter(mu_cl,np.zeros(mu.shape))
+    plt.scatter(mu_cl,np.zeros(mu.shape),s=winners_cl)
     plt.title('f_hat_b_cl')
 
     plt.subplot(2,2,4)
     plt.plot(x,f_hat_seq_cl,label="Predicted")
     plt.plot(x,label, label="True")
-    plt.scatter(mu_cl,np.zeros(mu.shape))
+    plt.scatter(mu_cl,np.zeros(mu.shape),s=winners_cl)
     plt.title('f_hat_seq_cl')
 
     plt.show()
@@ -127,26 +165,46 @@ if __name__ == "__main__":
     plt.subplot(2,2,1)
     plt.plot(valid,f_hat_b_test,label="Predicted")
     plt.plot(valid,label, label="True")
-    plt.scatter(mu, np.zeros(mu.shape))
+    plt.scatter(mu, np.zeros(mu.shape),s=winners)
     plt.title('f_hat_batch_test')
 
     plt.subplot(2,2,2)
     plt.plot(valid,f_hat_seq_test,label="Predicted")
     plt.plot(valid,label, label="True")
-    plt.scatter(mu,np.zeros(mu.shape))
+    plt.scatter(mu,np.zeros(mu.shape),s=winners)
     plt.title('f_hat_seq_test')
 
     plt.subplot(2,2,3)
     plt.plot(valid,f_hat_b_test_cl,label="Predicted")
     plt.plot(valid,label, label="True")
-    plt.scatter(mu_cl,np.zeros(mu.shape))
+    plt.scatter(mu_cl,np.zeros(mu.shape),s=winners_cl)
     plt.title('f_hat_b_batch_test')
 
     plt.subplot(2,2,4)
     plt.plot(valid,f_hat_seq_test_cl,label="Predicted")
     plt.plot(valid,label, label="True")
-    plt.scatter(mu_cl,np.zeros(mu.shape))
+    plt.scatter(mu_cl,np.zeros(mu.shape),s=winners_cl)
     plt.title('f_hat_b_cl_test')
 
 
     plt.show()
+
+
+def assignment1_ballist():
+    train_data, train_labels, test_data, test_labels = loadData()
+
+    no_of_nodes = 6
+
+    mu = []
+    ind = np.arange(0,train_data.shape[0])
+    for i in range(no_of_nodes):
+        index = np.random.choice(ind)
+        mu.append(train_data[index,:])
+    mu = np.array(mu)
+
+    mu_cl = clearning(train_data,mu)
+
+
+if __name__ == "__main__":
+    assignment1()
+    #assignment1_ballist()
