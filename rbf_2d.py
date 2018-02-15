aa77d0092c95eb765d9c0d63c4f99195b511d45e
Gabriel Carrizo <gabriel.carrizo@now.se>
2018-02-15 20:42:06 +0100

    cl 2d

--- rbf_2d.py
+++ rbf_2d.py
@@ -92,17 +92,46 @@ def seq_learn(x,label,mu,t, eta):
 
     return f, W
 
+def dist_(x, W, r=.5):
+
+    temp = []
+    for w in W:
+        d = x-w
+        d = d[0]**2 + d[1]**2
+        temp.append(d)
+
+    d = temp
+    in_radius = (d-np.min(d))<r
+
+    """
+    temp = np.subtract.outer(x,W)
+    d = temp**2
+    in_radius = np.abs(d-np.min(d)).T<r
+    """
+    return d, in_radius
+
+def clearning(x,mu_cl,t=50,eta=0.2,r=.5):
+    a = np.arange(x.shape[0])
+    for i in range(t):
+        x_samp = x[np.random.choice(a),:]
+        d, in_radius = dist_(x_samp, mu_cl,r=r)
+        mu_cl[np.argmin(d),:] += eta*(x_samp-mu_cl[np.argmin(d),:])
+    return mu_cl
+
 def assignment1_ballist():
     train_data, train_labels, test_data, test_labels = loadData()
 
-    no_of_nodes = 20
+    no_of_nodes = 5
 
     mu = []
     ind = np.arange(0,train_data.shape[0])
     for i in range(no_of_nodes):
         index = np.random.choice(ind)
         mu.append(train_data[index,:])
+
     mu = np.array(mu)
+    mu_cp = np.copy(mu)
+    mu_cl = clearning(train_data, mu_cp)
 
     t = 150
     eta = 0.2
@@ -110,14 +139,19 @@ def assignment1_ballist():
     f_hat_b, W_b = batch_train(train_data, train_labels, mu)
     f_hat_s, W_s = seq_learn(train_data, train_labels, mu, t, eta)
 
+    f_hat_b_cl, W_b_cl = batch_train(train_data, train_labels, mu)
+    f_hat_s_cl, W_s_cl = seq_learn(train_data, train_labels, mu, t, eta)
+
     #output space
     fig = plt.figure()
     tr = plt.scatter(train_labels[:,0],train_labels[:,1], c='g', label="true")
     f_h_s = plt.scatter(f_hat_s[:,0],f_hat_s[:,1], c='b', label="Seql")
     f_h_b = plt.scatter(f_hat_b[:,0], f_hat_b[:,1], c='r', label="Batch")
+    f_h_s_cl = plt.scatter(f_hat_s[:,0],f_hat_s[:,1], c='b', label="Seql cl")
+    f_h_b_cl = plt.scatter(f_hat_b[:,0], f_hat_b[:,1], c='r', label="Batch cl")
     plt.title('Batch and sequential: 150 epochs, eta: 0.2')
     plt.legend(handles=[f_h_s, f_h_b, tr])
-    fig.savefig('report/plots/2d/first_basic_both_no_CL_output')
+    #fig.savefig('report/plots/2d/first_basic_both_no_CL_output')
 
     plt.show()
 
@@ -125,9 +159,10 @@ def assignment1_ballist():
     fig2 = plt.figure()
     train_data = plt.scatter(train_data[:,0],train_data[:,1], c='g', label="data")
     nds = plt.scatter(mu[:,0],mu[:,1], c='b', label="Nodes")
-    plt.legend(handles=[train_data, nds])
+    nds_cl = plt.scatter(mu_cl[:,0],mu_cl[:,1], c='r', label="Nodes CL")
+    plt.legend(handles=[train_data, nds, nds_cl])
     plt.title("Training data and nodes")
-    fig2.savefig('report/plots/2d/input_basic_both_no_cl')
+    #fig2.savefig('report/plots/2d/input_basic_both_cl_INPUTAOSHAS')
 
     plt.show()
 
